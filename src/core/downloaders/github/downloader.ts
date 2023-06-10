5b3adb9bdf945d4f970f374909629601beb5e25f
shuhei kikuchi <qu1dpr09u0@gmail.com>
2023-06-10 13:24:35 +0900

    refactor(core): refactor GitHub and GitLab downloaders (#29)

--- src/core/downloaders/github/downloader.ts
+++ src/core/downloaders/github/downloader.ts
@@ -1,28 +1,10 @@
-import { $ } from 'execa';
+import { mkdir } from '../../../utils/mkdir';
+import { createFileComponents } from '../../file-components';
+import { createPossibleRefs } from '../../possible-refs';
+import { Downloader } from '../types';
+import { getArchive } from './get-archive';
 
-import { createFileComponents } from '../../utils/file-components';
-import { createPossibleRefs } from '../../utils/possible-refs';
-import { Downloader } from './types';
-
-async function getArchive({
-  repo,
-  ref,
-  redirectTo,
-}: {
-  repo: string;
-  ref?: string;
-  redirectTo: string;
-}) {
-  // https://docs.github.com/ja/rest/repos/contents?apiVersion=2022-11-28#download-a-repository-archive-zip
-  await $`gh api ${[
-    '-H',
-    'Accept: application/vnd.github+json',
-    '-H',
-    'X-GitHub-Api-Version: 2022-11-28',
-  ]} /repos/${repo}/zipball/${ref ?? ''}`.pipeStdout?.(redirectTo);
-}
-
-export const download: Downloader = async ({ repo, rest, archiveDir }) => {
+export const downloader: Downloader = async ({ repo, rest, archiveDir }) => {
   if (!rest) {
     const archiveDist = createFileComponents(`${archiveDir}/archive.zip`);
     await getArchive({ repo, redirectTo: archiveDist.filepath });
@@ -37,7 +19,7 @@ export const download: Downloader = async ({ repo, rest, archiveDir }) => {
 
   const { ref: resolvedRef, archive } = await Promise.any(
     possibleRefs.map(async (ref, i) => {
-      await $`mkdir ${archiveDir}/${i}`;
+      await mkdir(`${archiveDir}/${i}`);
       const archiveDist = createFileComponents(
         `${archiveDir}/${i}/archive.zip`
       );
